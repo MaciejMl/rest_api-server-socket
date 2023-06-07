6f751952845fa3c5e9d8c685f2e5b2dc2d46d8ef
MaciejMl <szypki@gmail.com>
2023-06-07 21:55:48 +0200

    Add server changes

--- server.js
+++ server.js
@@ -11,21 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
-
-app.use('/api', testimonials);
-app.use('/api', concerts);
-app.use('/api', seats);
-
-app.use(express.static(path.join(__dirname, '/client/public')));
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found...' });
-});
-
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
@@ -42,4 +27,23 @@ const io = socket(server, {
 io.on('connection', (socket) => {
   console.log('New socket!');
   socket.emit('seatsUpdated', seats);
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use('/api', testimonials);
+app.use('/api', concerts);
+app.use('/api', seats);
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
 });
